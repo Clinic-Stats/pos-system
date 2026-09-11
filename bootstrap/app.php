9b6337226412a1202d485d12daf22c370ec89195
karwan78 <karwan.karyy@gmail.com>
2026-09-11 04:16:47 +0300

    add trust proxies for vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-   ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'permission' => \App\Http\Middleware\CheckPermission::class,
         ]);
