9e3d91ab72302b619292208066ed54a8cabba30b
karwan78 <karwan.karyy@gmail.com>
2026-09-12 03:49:04 +0300

    گۆڕینی لۆگین لە ئیمەیڵەوە بۆ ناوی بەکارهێنەر

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,20 +18,19 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->validate([
-            'email'    => 'required|email',
+            'name'     => 'required|string',
             'password' => 'required',
         ]);
 
         if (Auth::attempt($credentials, $request->filled('remember'))) {
             $request->session()->regenerate();
 
-            // ئەگەر مەندووب بوو ڕاستەوخۆ دەچێتە سەر POS یان داشبۆردی خۆی
             return redirect()->intended(route('pos.index'))->with('success', 'بە سەرکەوتوویی چوویتە ژوورەوە');
         }
 
         return back()->withErrors([
-            'email' => 'ئیمەیڵ یان وشەی نهێنی هەڵەیە!',
-        ])->onlyInput('email');
+            'name' => 'ناوی بەکارهێنەر یان وشەی نهێنی هەڵەیە!',
+        ])->onlyInput('name');
     }
 
     public function logout(Request $request)
